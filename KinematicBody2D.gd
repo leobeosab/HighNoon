97daaa282974a8c61b9dc42fa2e38c4f038302a4
Cameron Wise <wisecameron1@gmail.com>
2020-05-25 08:23:07 -0400

    gun rotating to match cursor

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -10,14 +10,16 @@ func get_input():
 	var m_rotation = look_at(get_global_mouse_position())
 	rotation -= 45
 	if Input.is_action_just_pressed("mouse_down"):
+
 		shoot()
 
 	
 func shoot():
+
 	var b = Bullet.instance()
 	get_parent().add_child(b)
 	b.get_child(0).init($arm/Barrel.global_position, rotation - 45)
-
+	
 
 		
 
@@ -27,7 +29,9 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	$arm/gun.look_at(get_global_mouse_position())
 	get_input()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
